ed006c3b4cc2146acf14522869963f0123b37ea4
RussellDash332 <russellsaerang@gmail.com>
2023-05-04 16:55:37 +0800

    Scrape solved problems

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 import re
 import requests
+import seaborn as sns
 
+from bs4 import BeautifulSoup as bs
 from env import USER, PASSWORD
 
 class KattisSession(requests.Session):
@@ -26,7 +30,53 @@ class KattisSession(requests.Session):
             'password': self.password
         }
         response = self.post('https://open.kattis.com/login/email', data=data)
+        assert response.ok, 'Cannot login to Kattis'
         self.homepage = response.text
-        print(response.status_code)
+
+    def problems(self, show_solved=True, show_partial=True, show_tried=False, show_untried=False):
+        '''
+        Gets all Kattis problems based on some filters.
+        Returns a Pandas DataFrame with these columns:
+            name, fastest, shortest, total, acc, difficulty, category, link
+        '''
+
+        has_content = True
+        params = {
+            'page': 0,
+            'show_solved': ['off', 'on'][show_solved],
+            'show_partial': ['off', 'on'][show_partial],
+            'show_tried': ['off', 'on'][show_tried],
+            'show_untried': ['off', 'on'][show_untried]
+        }
+        data = []
+        while has_content:
+            has_content = False
+            response = self.get('https://open.kattis.com/problems', params=params)
+            soup = bs(response.content, features='lxml')
+            table = soup.find('table', class_='table2')
+            for row in table.tbody.find_all('tr'):    
+                columns = row.find_all('td')
+                if columns:
+                    has_content = True
+                    link = f"https://open.kattis.com{columns[0].find('a').get('href')}"
+                    name = columns[0].text
+                    fastest = float(columns[2].text)
+                    shortest = int(columns[3].text)
+                    total = int(columns[4].text)
+                    acc = int(columns[5].text)
+                    difficulty = float(re.findall('[\d\.]+', columns[7].text)[0])
+                        # [-1] instead of [0] if we want to take max instead of min
+                        # for example: difficulty 9.1-9.6
+                    category = re.findall('[A-Za-z]+', columns[7].text)[0]
+                    data.append([name, fastest, shortest, total, acc, difficulty, category, link])
+            params['page'] += 1
+        return pd.DataFrame(
+            columns=['name', 'fastest', 'shortest', 'total', 'acc', 'difficulty', 'category', 'link'],
+            data=data
+        )
 
 ks = KattisSession(USER, PASSWORD)
+data = ks.problems()
+print(data)
+sns.histplot(data.difficulty, binwidth=0.1)
+plt.show()
