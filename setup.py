75e6a982edd9226598b57ee9516f87a03ce9bf44
RussellDash332 <russellsaerang@gmail.com>
2023-05-11 19:25:18 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name='autokattis',
+    version='1.4',
+    description='Updated Kattis API wrapper',
+    long_description_content_type="text/markdown",
+    long_description=README,
+    license='MIT',
+    packages=find_packages(),
+    author='Russell Saerang',
+    author_email='russellsaerang@gmail.com',
+    keywords=['Kattis'],
+    install_requires = [
+        'requests',
+        'beautifulsoup4'
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'thefuzz',
+        'thefuzz[speedup]',
+    ],
+    url='https://github.com/RussellDash332/autokattis',
+    download_url='https://pypi.org/project/autokattis/'
+)
+
+if __name__ == '__main__':
+    setup(**setup_args, include_package_data=True)
